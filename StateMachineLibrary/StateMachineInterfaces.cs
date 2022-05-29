89e6f2ca270d977460ffd36eb01203e7786f7cd3
Винтер Яша <vinter-jasha@rambler.ru>
2022-05-29 20:49:20 +0500

    after refactoring

--- StateMachineLibrary/StateMachineInterfaces.cs
+++ StateMachineLibrary/StateMachineInterfaces.cs
@@ -23,11 +23,9 @@ namespace StateMachineLibrary
 
     public abstract class StateMachineBase
     {
-        public Dictionary<string, IState> stateDictionary { get; private protected set; } //= null;
-        public Dictionary<string, ITransition> transitionDictionary { get; private protected set; } // = null; // TODO разобратьс япочему есть доступ несмотря на protected
+        public Dictionary<string, IState> stateDictionary { get; private protected set; }
+        public Dictionary<string, ITransition> transitionDictionary { get; private protected set; }
         public string currentState { get; protected set; }
-        //protected abstract (Dictionary<string, IState> stateDictionary, Dictionary<string, ITransition> transitionDictionary)
-        //    DictionaryBilder(ISet<string> states, ISet<string> transitions);
         public StateMachineBase(IStateMashineFactory stateMashineFactory)
         {
             this.stateDictionary = stateMashineFactory.BuildStateDictionary();
@@ -55,29 +53,6 @@ namespace StateMachineLibrary
                         where t.transitionCriteria.InvokePredicate(command)
                         select t.transitionModel!.endState!.stateModel.name)
                        .FirstOrDefault();
-                //var v1 = stateDictionary[state].stateModel.transitions.Select(t => t.name);
-                //var v2 = (from tn in (from t in stateDictionary[state].stateModel.transitions select t.name) // v1
-                //          where transitionDictionary[tn].transitionCriteria.InvokePredicate(command)
-                //          select tn.Split(':')[1])
-                //         .FirstOrDefault();
-
-                //var v3 = (from t in transitionDictionary.Values
-                //         where t.transitionModel.entryState.name == state
-                //         where t.transitionCriteria.InvokePredicate(command)
-                //         select t.transitionModel.endState.name)
-                //         .FirstOrDefault();
-                //// worked
-                //return (from t1 in stateDictionary[state].stateModel.transitions //TODO mb remove warning
-                //        join t2 in transitionDictionary.Values on t1 equals t2.transitionModel
-                //        where t2.transitionCriteria.InvokePredicate(command)
-                //        select t2.transitionModel.endState.name)
-                //        .FirstOrDefault();
-
-                //// worked
-                //return transitionDictionary.Values
-                //    .Where(t => t.transitionModel.entryState.name == state && t.transitionCriteria.InvokePredicate(command))
-                //    .Select(t =>t.transitionModel.endState.name).FirstOrDefault();
-
             }
             catch (Exception)
             {
@@ -91,6 +66,5 @@ namespace StateMachineLibrary
         Dictionary<string, IState> BuildStateDictionary();
         Dictionary<string, ITransition> BuildTransitionDictionary();
         string BuildStartState();
-
     }
 }
--- StateMachineLibrary/StateMashine.cs
+++ StateMachineLibrary/StateMashine.cs
@@ -13,100 +13,6 @@ namespace StateMachineLibrary
         public StateMachine(IStateMashineFactory stateMashineFactory) : base(stateMashineFactory)
         {
         }
-        //public StateMachine(IEnumerable<IState> states, IEnumerable<ITransition> transitions, string startState)
-        //{
-        //    this.stateDictionary = states.ToDictionary(s => s.stateModel.name);
-        //    this.transitionDictionary = transitions.ToDictionary(t => t.transitionModel.name);
-        //    //this.stateDictionary = new Dictionary<string,IState>(
-        //    //    from s in states
-        //    //    select new KeyValuePair<string, IState>(s.stateModel.name, s));
-        //    //this.transitionDictionary = new Dictionary<string, ITransition>(
-        //    //    from t in transitions
-        //    //    select new KeyValuePair<string, ITransition>(t.transitionModel.name, t));
-        //    this.currentState = startState;
-        //}
-        //public StateMachine(ISet<string> states, ISet<string> transitions, string startState)
-        //{
-        //    //creates stateDictionary and transitionDictionary
-        //    (this.stateDictionary, this.transitionDictionary) = DictionaryBilder(states, transitions);
-        //    this.currentState = startState;
-        //}
-        ///// <summary>
-        ///// initialaze stateDictionary and transitionDictionary
-        ///// </summary>
-        //protected override (Dictionary<string, IState> stateDictionary, Dictionary<string, ITransition> transitionDictionary)
-        //    DictionaryBilder(ISet<string> stateNames, ISet<string> transitionNames)
-        //{
-        //    var stateDictionary = new Dictionary<string, IState>();
-        //    var transitionDictionary = new Dictionary<string, ITransition>();
-        //    /// create stateDictionary
-        //    foreach (var stateName in stateNames)
-        //    {
-        //        State state = new State(new StateModel(stateName), new StateEvent(null), new StateData(null, null)); //new StateData()
-        //        stateDictionary.Add(stateName, state);
-        //    }
-        //    /// create transitionDictionary
-        //    foreach (var transitionName in transitionNames)
-        //    {
-        //        var names = transitionName.Split(new char[] { ':' });
-        //        string entryStateName = names[0];
-        //        string endStateName = names[1];
-        //        State entryState = stateDictionary[entryStateName] as State;
-        //        State endState = stateDictionary[endStateName] as State;
-
-        //        TransitionModel transitionModel = new TransitionModel(transitionName)
-        //        {
-        //            entryState = entryState.stateModel,
-        //            endState = endState.stateModel
-        //        };
-        //        Transition transition = new Transition(transitionModel, new TransitionCriteria(null));
-        //        transitionDictionary.Add(transitionName, transition);
-        //    }
-        //    //add transitions to every state
-        //    foreach (var state in stateDictionary.Values)
-        //    {
-        //        var foundTransitions = transitionDictionary.Values
-        //            .Where(t => t.transitionModel.entryState.name == state.stateModel.name)
-        //            .Select(t => t.transitionModel).ToHashSet();
-        //        //var foundTransitions = transitionDictionary.Values.Select(x => x).Where(x => x.entryState.name == state.name).ToHashSet();
-        //        state.stateModel.transitions = foundTransitions;
-        //        //state.transitions = foundTransitions;
-        //    }
-        //    return (stateDictionary, transitionDictionary);
-        //}
-        //public void AddFunctionHandler(Dictionary<string, FunctionHandler> actionsDictionary)
-        //{
-        //    if (actionsDictionary is null)
-        //        throw new ArgumentNullException();
-        //    foreach (var actionsPair in actionsDictionary)
-        //    {
-        //        string state = actionsPair.Key;
-        //        FunctionHandler functionHandler = actionsPair.Value;
-        //        stateDictionary[state].stateEvent.functionHandler += functionHandler;
-        //    }
-        //}
-        //public void AddCriteraRange(Dictionary<string, Predicate<string>> criteriaDictionary)
-        //{
-        //    if (criteriaDictionary is null)
-        //        throw new ArgumentNullException();
-        //    foreach (var criteriaPair in criteriaDictionary)
-        //    {
-        //        string transition = criteriaPair.Key;
-        //        Predicate<string> criteria = criteriaPair.Value;
-        //        transitionDictionary[transition].transitionCriteria.criteria += criteria;
-        //    }
-        //}
-        //public void AddEventData(Dictionary<string, EventDataBase> eventDataDictionary)
-        //{
-        //    if (eventDataDictionary is null)
-        //        throw new ArgumentNullException();
-        //    foreach (var eventDataPair in eventDataDictionary)
-        //    {
-        //        string state = eventDataPair.Key;
-        //        var eventData = eventDataPair.Value;
-        //        stateDictionary[state].stateData.eventData = eventData;
-        //    }
-        //}
         public override object Execute(InputDataBase InputData) // InputDataBase
         {
             string? nextState = CheckTransitions(currentState, InputData.command);
@@ -138,4 +44,3 @@ namespace StateMachineLibrary
         EventError
     }
 }
-// before refactoring
